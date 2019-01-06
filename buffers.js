9ab191a0207d9b1ecae89354fd0b0d2d3e3f6a30
beginner <me@example.com>
2019-01-05 19:16:13 -0800

    buffers example 4

--- buffers.js
+++ buffers.js
@@ -1,4 +1,5 @@
-var buf = new Buffer('Simply Easy Learning');
-var json = buf.toJSON(buf);
+var buffer1 = new Buffer('TutorialsPoint ');
+var buffer2 = new Buffer('Simply Easy Learning');
+var buffer3 = Buffer.concat([buffer1,buffer2]);
 
-console.log(json);
+console.log("buffer3 content: " + buffer3.toString());
