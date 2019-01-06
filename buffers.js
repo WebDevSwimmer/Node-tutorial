e15b717cb00a6813e1bc749d98c38bfe11e4a376
beginner <me@example.com>
2019-01-05 19:12:23 -0800

    buffers example2

--- buffers.js
+++ buffers.js
@@ -1,4 +1,9 @@
-buf = new Buffer(256);
-len = buf.write("Simply Easy Learning");
+buf = new Buffer(26);
+for (var i = 0 ; i < 26 ; i++) {
+  buf[i] = i + 97;
+}
 
-console.log("Octets written : "+  len);
+console.log( buf.toString('ascii'));       // outputs: abcdefghijklmnopqrstuvwxyz
+console.log( buf.toString('ascii',0,5));   // outputs: abcde
+console.log( buf.toString('utf8',0,5));    // outputs: abcde
+console.log( buf.toString(undefined,0,5)); // encoding defaults to 'utf8', outputs abcde
