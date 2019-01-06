5a29264124ebd9823875b3e2bd21c226a8be9ab1
beginner <me@example.com>
2019-01-05 19:18:29 -0800

    buffers example 6

--- buffers.js
+++ buffers.js
@@ -1,11 +1,6 @@
 var buffer1 = new Buffer('ABC');
-var buffer2 = new Buffer('ABCD');
-var result = buffer1.compare(buffer2);
 
-if(result < 0) {
-   console.log(buffer1 +" comes before " + buffer2);
-} else if(result === 0) {
-   console.log(buffer1 +" is same as " + buffer2);
-} else {
-   console.log(buffer1 +" comes after " + buffer2);
-}
+//copy a buffer
+var buffer2 = new Buffer(3);
+buffer1.copy(buffer2);
+console.log("buffer2 content: " + buffer2.toString());
