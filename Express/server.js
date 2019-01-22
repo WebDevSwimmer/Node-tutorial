b489805e10309e7b715f7469cd01a8bff4738a0c
beginner <me@example.com>
2019-01-21 19:51:05 -0800

    Express

--- Express/server.js
+++ Express/server.js
@@ -1,8 +1,67 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser');
+var multer  = require('multer');
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.get('/get.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "get.html" );
+})
+app.get('/post.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "post.html" );
+})
+
+app.get('/upload.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/file_upload', function (req, res) {
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
+   
+   fs.readFile( req.files.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ) {
+            console.log( err );
+            } else {
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files.file.name
+               };
+            }
+         
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 })
 
 var server = app.listen(8081, function () {
